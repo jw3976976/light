dd4f1c4ec917d75afab019743b9e46bd962919c9
jw3976976 <2675739145@qq.com>
2020-06-15 15:59:27 +0800

    new

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+//import Home from '../views/home/index.vue'
+//使用按需引用的方式，并不是很完善，使用时需要使用一些插件，如@babel/plugin
+//使用组件按需引入的方式，只有组件激活了才会渲染组件的内容
+const Home = ()=>import('../views/home/index.vue')
 
 Vue.use(VueRouter)
 
@@ -10,14 +13,14 @@ Vue.use(VueRouter)
     name: 'Home',
     component: Home
   },
-  {
+  /*{
     path: '/about',
     name: 'About',
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    component: () => import(/* webpackChunkName: "about" *\/ '../views/About.vue')
+  }*/
 ]
 
 const router = new VueRouter({
